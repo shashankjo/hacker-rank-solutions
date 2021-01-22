25249149eee8e28681980171ae0984bf47eb9ebb
Kumar Shashank <kumarshashank.jo@gmail.com>
2021-01-22 11:38:04 +0530

    Solution: Mini max sum

---
+++ mini_max_sum.rb
@@ -0,0 +1,20 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr)
+ arr=arr.sort 
+ mini=arr.first
+ max=arr.last
+ min_sum=arr.take(4).sum
+ max_sum=arr.drop(1).sum
+ out=[]
+ out << min_sum
+ out << max_sum
+ puts out.join(" ")
+end
+arr = gets.rstrip.split(' ').map(&:to_i)
+
+miniMaxSum arr
