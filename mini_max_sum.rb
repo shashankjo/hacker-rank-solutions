613e710d7de52b0b33a1ee0564d7edbfaf4bad92
unknown <kumarshashank.jo@gmail.com>
2021-10-20 13:15:56 +0530

    Refactor code

--- mini_max_sum.rb
+++ mini_max_sum.rb
@@ -5,15 +5,15 @@ require 'stringio'
 
 # Complete the miniMaxSum function below.
 def miniMaxSum(arr)
-	arr = arr.sort
-	mini = arr.first
-	max = arr.last
-	min_sum = arr.take(4).sum
-	max_sum = arr.drop(1).sum
-	output = []
-	output << min_sum
-	output << max_sum
-	puts output.join(" ")
+  arr = arr.sort
+  mini = arr.first
+  max = arr.last
+  min_sum = arr.take(4).sum
+  max_sum = arr.drop(1).sum
+  output = []
+  output << min_sum
+  output << max_sum
+  puts output.join(" ")
 end
 arr = gets.rstrip.split(' ').map(&:to_i)
 
